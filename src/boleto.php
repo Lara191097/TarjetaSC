e378cfd5ab40b22d203ac818d170bd39dedba877
Lara191097 <laravictoriarodriguez@gmail.com>
2016-11-09 14:32:58 -0300

    Create boleto.php

---
+++ src/boleto.php
@@ -0,0 +1,20 @@
+<?php
+namespace Poli\Tarjeta;
+class Boleto { 
+		
+	
+  public function mostrarinfo () {
+     return $this->tarjeta->tipo;
+     return $this->tarjeta->nombre;
+     return $this->tarjeta->tipotransporte;
+     return $this->colectivo->darnombre;
+     return $this->viaje->dardia;
+     return $this->viaje->darfecha;
+     return $this->viaje->darhora;
+     return $this->viaje->darmonto;
+     return $this->tarjeta->saldo;
+  }
+  
+  
+	
+   } 
