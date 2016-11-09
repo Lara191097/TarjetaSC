3402179e911b47d5a5546ef63d4a0f83c3f0ea9a
Lara191097 <laravictoriarodriguez@gmail.com>
2016-11-09 14:35:32 -0300

    Update Tarjeta.php

--- src/Tarjeta.php
+++ src/Tarjeta.php
@@ -1,11 +1,9 @@
+
 <?php
-
 namespace Poli\Tarjeta;
-
 interface Tarjeta{
-	public function pagar($transporte, $hora, $fecha);
+	public function pagar($transporte,$dia, $hora, $fecha);
 	public function recargar($monto);
 	public function saldo();
-	public function viajesRealizados();
+	
 }
-
