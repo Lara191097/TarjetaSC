a8d95ad853558dd74940a25787bb9f955b31247c
Lara191097 <laravictoriarodriguez@gmail.com>
2016-11-08 23:03:38 -0300

    Update Test.php

--- tests/Test.php
+++ tests/Test.php
@@ -22,15 +22,27 @@ class TarjetaTest extends TestCase {
 	$this->assertEquals($tarje->saldo(), (320-4), "Cuando cargo 272 deberia tener finalmente 320 y paga 4 de pasaje");
 
   }
-	public function testMedio() {
+	
+ public function testnMedio() {
 	$bondi= new Colectivos("144");
 	$tarje= new Tarjetas("estudiante", "Medio boleto");
 	$tarje->recargar(272);
-	$tarje->pagar($bondi,"18.52","miercoles","19/10/2016");
+	$tarje->pagar($bondi,"18.52","lunes","15/09/2016");
 	$this->assertEquals($tarje->saldo(), 316, "Cuando cargo 272 deberia tener finalmente 320 y paga 4 de pasaje");
 	$this->assertEquals($tarje->boleto->darmonto(), 4,"Paga 4 de pasaje");
-  }
 
 
-
+}
+	public function testColectivo() {	
+   	$tarje= new Tarjetas("estudiante", "Medio");
+	$tarje->recargar(288);
+	$bondi= new Colectivos("144");
+	$tarje->pagar($bondi,"18.52","martes","15/09/2016");  
+	$this->assertEquals($tarje->boleto->dartransporte(),"144", "Utiliza el colectivo 144");
+	$this->assertEquals($tarje->boleto->darhora(),"18.52", "Lo uso a las 18.52");
+	$this->assertEquals($tarje->boleto->darmonto(),"4", "Pago un monto de 4 pesos");
+	$this->assertEquals($tarje->boleto->darfecha(),"martes", "Utiliza el colectivo el dia martes");
+	$this->assertEquals($tarje->boleto->darsaldo(),284, "la tarjeta tiene un saldo de 284");
+	$this->assertEquals($tarje->boleto->dartipoviaje(),"Medio", "el viaje es del tipo estudiante");
+   }
 }
