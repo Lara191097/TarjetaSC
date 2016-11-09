cac9c9765b59ed4531f1a80211a4f39b0691b0ff
Lara191097 <laravictoriarodriguez@gmail.com>
2016-11-09 14:39:12 -0300

    Update and rename Viaje.php to Viajes.php

---
+++ src/Viajes.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Poli\Tarjeta;
+
+class Viajes{
+	protected $nombreultimotransporteusado;
+	protected $horaultimoviajehecho;
+	protected $fechaultimoviaje;
+	protected $diadefecha;
+	protected $monto;
+	protected $boleto;
+	public function __construct(){
+		$this->boleto= new Boleto;
+	}
+	public function darnombre(){
+		return $this->nombreultimotransporteusado;
+	}
+	public function darhora(){
+		return $this->horaultimoviajehecho;
+	}
+	public function darfecha(){
+		return $this->fechaultimoviaje;
+	}
+	public function dardia(){
+		return $this->diadefecha;
+	}
+	public function darmonto(){
+		return $this->monto;
+	}
+	public function pedirdatosultimoviaje($nombre,$monto,$dia,$fecha,$hora){
+		$this->nombreultimotransporteusado=$nombre;
+		$this->monto=$monto;
+		$this->diadefecha=$dia;
+		$this->fechaultimoviaje=$fecha;
+		$this->horaultimoviajehecho=$hora;
+		
+	}
+}
